cdf3cd10fee9134021ce5f538689ead1c6198696
camalzade_elvin <camalzade_elvin@outlook.com>
2019-07-08 20:42:25 +0400

    Add project files.

---
+++ ScreenShot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ScreenShot
+{  
+    class Program
+    {
+        private static void takeScreenShot()
+        {
+            Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.CopyFromScreen(0, 0, 0, 0, Screen.PrimaryScreen.Bounds.Size);
+                bmp.Save("screenshot.png");  // saves the image
+            }
+        }
+        static void Main(string[] args)
+        {
+            takeScreenShot();
+        }
+    }
+}
